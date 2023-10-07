4a16b07236fc59bdcd324fdb9db5e993275edfad
Kaybee973 <ewejeabdulkabir@gmail.com>
2023-10-06 18:40:37 -0700

    Task 5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+/**
+ * main: the starting point
+ * return (0) - SUCCESS
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 /* Writing program on printf */
 #include <stdio.h>
 
-/** 
+/**
  * main - Starting and ending point
  *
  * Returm: (0) = SUCCESS
 */
-int main (void)
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
