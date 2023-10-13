4af18f2d27355712f8dbcdbeef2059cf84c0308c
Kaybee973 <ewejeabdulkabir@gmail.com>
2023-10-13 16:39:53 -0700

    Dubugging

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -7,3 +10,10 @@ int print_sign(int n);
 int _abs(int );
 int print_last_digit(int);
 void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
+
+
+#endif
