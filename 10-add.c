5e15f9cc02d831774ef2347228fe84634bea2fc2
Kaybee973 <ewejeabdulkabir@gmail.com>
2023-10-13 16:23:25 -0700

    Debugging

---
+++ 10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result
+ * @a: int inputed
+ * @b: int inputed
+ * Return: int c
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+
+	return (c);
+}
