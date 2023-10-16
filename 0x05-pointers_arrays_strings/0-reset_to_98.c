e45e88236f879d4f30576f77f7c6b9c1a506efad
Kaybee973 <ewejeabdulkabir@gmail.com>
2023-10-16 09:47:51 -0700

    First commit

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,16 +2,12 @@
 
 /**
  * reset_to_98 - check main
- * @*n: An input pointer
+ * @n: An input pointer
  * Description: function that takes a pointer to an int as parameter
  * Return: void
  */
 
 void reset_to_98(int *n)
 {
-	int a;
-	
-	n = &a;
-	a = 98;
-
+	*n = 98;
 }
