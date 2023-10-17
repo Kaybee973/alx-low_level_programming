1d0c2805fbfc2f20cdeb8e5c07193ecc29d4f4e3
Kaybee973 <ewejeabdulkabir@gmail.com>
2023-10-17 16:36:20 -0700

    print 2nd half string

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -3,7 +3,9 @@
 /**
  * puts2 - check main
  * @str: An input string pointer
- * Description: function that prints every other character of a string, starting with the first character, followed by a new line.
+ * Description: function that prints every other
+ * character of a string, starting
+ * with the first character, followed by a new line.
  * Return: void
  */
 
