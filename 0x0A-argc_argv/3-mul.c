d6c4468f4d81aa499bccc3aa02fd926e93710286
Kaybee973 <ewejeabdulkabir@gmail.com>
2023-10-31 03:29:43 -0700

    Multiplication of argv[1] and argv[2]

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 /**
  * main - Entry point
  * @argc: count the arguments supplied to the program
  * @argv: an array of pointers to the strings
- * Description: program that prints all arguments it receives
+ * Description: program that prints result of multiplication
  * Return: Always 0 (Success)
  */
 
 int main(int argc, char *argv[])
 {
-	int i;
-
-	for (i = 0; i < argc; i++)
-		printf("%s\n", argv[i]);
-
+	if (argc > 2)
+		printf("%d\n", (atoi(argv[1]) * atoi(argv[2])));
+	else
+	printf("Error\n");
 	return (0);
 }
