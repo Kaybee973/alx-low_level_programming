b347982f0158d63b8b914e8d37d117694e7f3a03
Kaybee973 <ewejeabdulkabir@gmail.com>
2023-10-30 22:15:23 -0700

    Printing orogram name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: Program that prints its name, with new line
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
