bcced9bc4cf87f408afded9040999445729f71b7
Kaybee973 <ewejeabdulkabir@gmail.com>
2023-11-01 15:27:05 -0700

    returns pointer space

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * create_array - function that creates an array of chars, and initializes it with a specific char
+ * @size: the length of the array
+ * @c: input character
+ * Return: void pointer
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *void_pointer;
+	unsigned int i;
+
+	if (size == 0)
+		return (NULL);
+
+	void_pointer = (char *) malloc(sizeof(char) *size);
+	if (void_pointer == NULL)
+		return (NULL);
+
+
+	for (i = 0; i < size; i++)
+		void_pointer[i] = c;
+
+	return (void_pointer);
+}
