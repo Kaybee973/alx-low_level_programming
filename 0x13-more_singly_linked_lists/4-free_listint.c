fba404e1cede9a2ce0d7fe4c21cf1361ecacaa2c
Kaybee973 <ewejeabdulkabir@gmail.com>
2023-11-21 14:55:10 -0800

    frees a node

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_listint - frees a listint_t list
+ *
+ * @head: head of linked list
+ *
+ * Return: nothing
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *next;
+
+	while (head != NULL)
+	{
+		next = head->next;
+		free(head);
+		head = next;
+	}
+}
